f81a958d69a1f9f451516635f802f006bff42ed4
Pooja Kanojia <56120111+Pooja123667@users.noreply.github.com>
2021-12-22 22:08:22 +0530

    Create Trapping-rainwater.py

---
+++ Trapping-rainwater.py
@@ -0,0 +1,29 @@
+# python questions
+# Trapping rain water
+
+# Function to return the maximum
+# water that can be stored
+def maxWater(arr, n) :
+     
+    # To store the maximum water
+    # that can be stored
+    res = 0;
+     
+    # For every element of the array
+    for i in range(1, n - 1) :
+         
+        # Find the maximum element on its left
+        left = arr[i];
+        for j in range(i) :
+            left = max(left, arr[j]);
+         
+        # Find the maximum element on its right
+        right = arr[i];
+         
+        for j in range(i + 1 , n) :
+            right = max(right, arr[j]);
+             
+        # Update the maximum water
+        res = res + (min(left, right) - arr[i]);
+ 
+    return res;
